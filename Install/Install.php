de28d4f0561d47a1453ee93c32b6c877362d9fef
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-02-09 23:20:21 +1100

    Add menu for dashboards

--- Install/Install.php
+++ Install/Install.php
@@ -3,11 +3,30 @@
 namespace Apps\Fintech\Components\Home\Install;
 
 use System\Base\BasePackage;
+use System\Base\Providers\ModulesServiceProvider\MenuInstaller;
 
 class Install extends BasePackage
 {
+    protected $menuInstaller;
+
+    public function init()
+    {
+        $this->menuInstaller = new MenuInstaller;
+
+        return $this;
+    }
+
     public function install()
     {
+        $this->installMenu();
+
+        return true;
+    }
+
+    protected function installMenu()
+    {
+        $this->menuInstaller->installMenu($this);
+
         return true;
     }
 
